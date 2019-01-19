7954c99a6131f83bcc2c34a1234d3e58d4b55f57
Abrehamgit <gezahegnabereham@gmail.com>
2019-01-19 05:25:39 -0800

    .env and post urls in th

--- controllers/profile.js
+++ controllers/profile.js
@@ -16,7 +16,17 @@ const handlePost = (db, jwt) => (req, res) => {
 		tiles,
 		ceramics,
 		spotlights,
-		featured
+		featured,
+		image1,
+		image2,
+		image3,
+		image4,
+		image5,
+		image6,
+		image7,
+		image8,
+		image9,
+		image10
 	} = req.body;
 
 	db.insert({
@@ -35,7 +45,17 @@ const handlePost = (db, jwt) => (req, res) => {
 		tiles,
 		ceramics,
 		spotlights,
-		featured
+		featured,
+		image1,
+		image2,
+		image3,
+		image4,
+		image5,
+		image6,
+		image7,
+		image8,
+		image9,
+		image10
 	})
 		.into("condominiums")
 		.returning("*")
